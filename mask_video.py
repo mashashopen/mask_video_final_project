5b35c0fab3c81c803cc2c0b462364b8b2676ed22
mashashopen <112798347+mashashopen@users.noreply.github.com>
2023-07-16 10:34:24 +0300

    export data to csv file and analysis

--- mask_video.py
+++ mask_video.py
@@ -1,3 +1,5 @@
+import csv
+
 import cv2
 import os
 from extract_frames import ExtractFrames
@@ -17,7 +19,7 @@ class MaskVideo:
 
         # Create a VideoWriter object to write the frames to a video file
         self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        self.video = cv2.VideoWriter(self.video_file_name + '-masked.mp4', self.fourcc, self.fps,
+        self.video = cv2.VideoWriter(self.video_file_name[:-4] + '-masked.mp4', self.fourcc, self.fps,
                                       self._get_width_height())
 
     def _get_width_height(self) -> tuple:
@@ -29,16 +31,47 @@ class MaskVideo:
     def _get_unmasked_sorted_frames(self) -> list:
         return self.extract_frames_manager.sorted_frames_files()
 
+    def extract_data_to_csv_file(self, data, unmasked_frames):
+        # Specify the field names (column names) for the CSV file
+        fieldnames = ["number_of_frame", "x1", "y1", "x2", "y2"]
+
+        filename_without_extension = self.video_file_name[:-4]
+        # Specify the file name for the CSV file
+        filename = filename_without_extension + ".csv"
+
+        # Open the CSV file in write mode
+        with open(filename, "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            # Add comments or metadata to the CSV file
+            csvfile.write(self.extract_frames_manager.get_video_dimensions() + "\n")
+            csvfile.write("Number of frames:" + str(len(unmasked_frames)) + "\n")
+            # Write the header (field names)
+            writer.writeheader()
+            # Write the data rows
+            for row in data:
+                writer.writerow(row)
+            writer.writerow(row)
+        print("CSV file exported successfully.")
+
     def mask_video_flow(self):
         unmasked_frames = self._get_unmasked_sorted_frames()
+        rows = []
+
         with tqdm(total=len(unmasked_frames), desc="Progress", unit="frame") as pbar:
             for i, frame in enumerate(unmasked_frames):
                 full_path = self.extract_frames_manager.get_unmasked_dir_name() + "/" + frame
                 mask_frame_manager = MaskFrame(full_path)
+                for face in mask_frame_manager.get_all_faces_locations():
+                    row = {"number_of_frame": i, "x1": face[0], "y1": face[1], "x2": face[2], "y2": face[3]}
+                    rows.append(row)
                 masked_frame = mask_frame_manager.mask_frame(self.kernel_size, self.epsilon)
                 self.video.write(masked_frame)
                 pbar.update(1)
         self.video.release()
+
+        self.extract_data_to_csv_file(rows, unmasked_frames)
+
+
 
 
 root = Tk()
@@ -69,7 +102,7 @@ entry_video_file.pack()
 button_browse = Button(root, text="Browse", command=browse_video_file)
 button_browse.pack()
 
-button_process = Button(root, text="Mask Video", command=process_video)
+button_process = Button(root, text="Process Video", command=process_video)
 button_process.pack()
 
 root.mainloop()
