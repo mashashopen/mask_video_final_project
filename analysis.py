f3cd630011e0e9c64e907dcb4c613fa58a49c889
mashashopen <112798347+mashashopen@users.noreply.github.com>
2023-09-13 01:10:40 +0300

    analysis script takes arguments

--- analysis.py
+++ analysis.py
@@ -1,56 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-df = pd.read_csv('world.csv', skiprows=2)
-
-first_row = pd.read_csv('world.csv', nrows=1).to_string(index=False)
-
-# Step 4: Extract the frame size
-size = first_row[first_row.find(":") + 2:first_row.find("\n")]
-frame_width = int(size[:size.find("x")])
-frame_height = int(size[size.find("x") + 1:])
-
-# Step 5: Initialize an empty heatmap
-heatmap = np.zeros((frame_height, frame_width))
-
-# Step 6: Update the heatmap based on face locations
-for index, row in df.iterrows():
-    x1, y1, x2, y2 = row['x1'], row['y1'], row['x2'], row['y2']
-    x1 = int(x1)
-    y1 = int(y1)
-    x2 = int(x2)
-    y2 = int(y2)
-    heatmap[y1:y2, x1:x2] += 1
-
-# Step 7: Plot the heatmap
-plt.imshow(heatmap, cmap='hot')
-plt.colorbar(label='Frequency')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Heatmap of Face Locations')
-plt.show()
+import sys
 
 
+def main():
+    csv_file = sys.argv[1]
 
-face_data = df[['x1', 'y1', 'x2', 'y2']]
+    # Step 1: Read csv file
 
-# Step 5: Scale the face coordinates based on the frame size
-face_data['x1'] = face_data['x1'] / frame_width
-face_data['y1'] = face_data['y1'] / frame_height
-face_data['x2'] = face_data['x2'] / frame_width
-face_data['y2'] = face_data['y2'] / frame_height
+    df = pd.read_csv(csv_file, skiprows=2)
 
-# Step 6: Calculate the average location of the faces
-average_location = face_data.mean()
+    first_row = pd.read_csv(csv_file, nrows=1).to_string(index=False)
 
-# Step 7: Plot the average location
-plt.figure(figsize=(6, 6))
-plt.scatter(average_location['x1'], average_location['y1'], c='red', marker='o')
-plt.xlabel('X (Normalized)')
-plt.ylabel('Y (Normalized)')
-plt.title('Average Location of Faces (Normalized)')
-plt.xlim(0, 1)  # Set the X-axis limits to match the normalized coordinates
-plt.ylim(0, 1)  # Set the Y-axis limits to match the normalized coordinates
-plt.gca().invert_yaxis()  # Invert the Y-axis to match the video frame
-plt.show()
+    # Step 2: Extract the frame size
+    size = first_row[first_row.find(":") + 2:first_row.find("\n")]
+    frame_width = int(size[:size.find("x")])
+    frame_height = int(size[size.find("x") + 1:])
+
+    # Step 3: Initialize an empty heatmap
+    heatmap = np.zeros((frame_height, frame_width))
+
+    # Step 4: Update the heatmap based on face locations
+    for index, row in df.iterrows():
+        x1, y1, x2, y2 = row['x1'], row['y1'], row['x2'], row['y2']
+        x1 = int(x1)
+        y1 = int(y1)
+        x2 = int(x2)
+        y2 = int(y2)
+        heatmap[y1:y2, x1:x2] += 1
+
+    # Step 5: Plot the heatmap
+    plt.imshow(heatmap, cmap='hot')
+    plt.colorbar(label='Frequency')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Heatmap of Face Locations')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- multi_analysis.py
+++ multi_analysis.py
@@ -1,47 +1,55 @@
+import sys
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# List of CSV files
-csv_files = ['world.csv', 'camatim_short.csv']
 
-# Initialize empty lists for average locations
-average_locations_x = []
-average_locations_y = []
+def main():
+    # List of CSV files
+    csv_files = sys.argv[1:]
 
-# Iterate over each CSV file
-for file in csv_files:
-    first_row = pd.read_csv(file, nrows=1).to_string(index=False)
-    size = first_row[first_row.find(":") + 2:first_row.find("\n")]
-    frame_width = int(size[:size.find("x")])
-    frame_height = int(size[size.find("x") + 1:])
+    # Initialize empty lists for average locations
+    average_locations_x = []
+    average_locations_y = []
 
-    # Step 3: Read the CSV file into a pandas DataFrame
-    df = pd.read_csv(file, skiprows=2)
+    # Iterate over each CSV file
+    for file in csv_files:
+        first_row = pd.read_csv(file, nrows=1).to_string(index=False)
+        size = first_row[first_row.find(":") + 2:first_row.find("\n")]
+        frame_width = int(size[:size.find("x")])
+        frame_height = int(size[size.find("x") + 1:])
 
-    # Step 4: Extract the face location data
-    face_data = df[['x1', 'y1', 'x2', 'y2']]
+        # Step 3: Read the CSV file into a pandas DataFrame
+        df = pd.read_csv(file, skiprows=2)
 
-    # Step 5: Scale the face coordinates based on the frame size
+        # Step 4: Extract the face location data
+        face_data = df[['x1', 'y1', 'x2', 'y2']]
 
-    face_data['x1'] = face_data['x1'] / frame_width
-    face_data['y1'] = face_data['y1'] / frame_height
-    face_data['x2'] = face_data['x2'] / frame_width
-    face_data['y2'] = face_data['y2'] / frame_height
+        # Step 5: Scale the face coordinates based on the frame size
 
-    # Step 6: Calculate the average location for each file
-    average_location = face_data.mean()
+        face_data.loc[:, 'x1'] = face_data['x1'] / frame_width
+        face_data.loc[:, 'y1'] = face_data['y1'] / frame_height
+        face_data.loc[:, 'x2'] = face_data['x2'] / frame_width
+        face_data.loc[:, 'y2'] = face_data['y2'] / frame_height
 
-    # Append average locations to lists
-    average_locations_x.append(average_location['x1'])
-    average_locations_y.append(average_location['y1'])
+        # Step 6: Calculate the average location for each file
+        average_location = face_data.mean()
 
-# Plot the average locations for all files
-plt.figure(figsize=(6, 6))
-plt.scatter(average_locations_x, average_locations_y, c='red', marker='o')
-plt.xlabel('X (Normalized)')
-plt.ylabel('Y (Normalized)')
-plt.title('Average Location of Faces')
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-plt.gca().invert_yaxis()
-plt.show()
+        # Append average locations to lists
+        average_locations_x.append(average_location['x1'])
+        average_locations_y.append(average_location['y1'])
+
+    # Plot the average locations for all files
+    plt.figure(figsize=(6, 6))
+    plt.scatter(average_locations_x, average_locations_y, c='red', marker='o')
+    plt.xlabel('X (Normalized)')
+    plt.ylabel('Y (Normalized)')
+    plt.title('Average Location of Faces')
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.gca().invert_yaxis()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
